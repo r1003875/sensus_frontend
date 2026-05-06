d0275337fad31588f86ae04f1c3d880297504c55
Simon Van den Eynde <r1003875@student.thomasmore.be>
2026-05-06 17:35:34 +0200

    feat: replace logo image source in Start.vue with local SVG file

--- Sensus_FrontEnd/src/views/Start.vue
+++ Sensus_FrontEnd/src/views/Start.vue
@@ -11,7 +11,7 @@ const startApp = () => router.push('/code')
   <main class="app-screen">
     <section class="start-screen">
       <div class="start-screen__logo">
-        <img src="https://www.figma.com/api/mcp/asset/ee3f27a5-cf39-42e6-8128-e30f001a4941" alt="" />
+        <img src="../assets/images/sensus-logo.svg" alt="Sensus Logo" />
       </div>
       <h1>Oefen met grenzen en consent</h1>
       <p>Ontdek realistische situaties en leer hoe jij op een veilige, respectvolle manier kan reageren.</p>
