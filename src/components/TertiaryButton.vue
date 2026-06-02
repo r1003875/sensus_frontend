0c16304f5531fc06f869dd617b0955586dacf273
Simon Van den Eynde <r1003875@student.thomasmore.be>
2026-06-02 11:09:30 +0200

    feat: add TertiaryButton component with styling for tertiary button variant

---
+++ src/components/TertiaryButton.vue
@@ -0,0 +1,33 @@
+<script setup>
+import BaseButton from '@/components/BaseButton.vue'
+
+defineOptions({
+	inheritAttrs: false,
+})
+
+defineProps({
+	text: {
+		type: String,
+		default: '',
+	},
+})
+
+defineEmits(['click'])
+</script>
+
+<template>
+	<BaseButton
+		variant="link"
+		class="tertiary-btn"
+		:text="text"
+		v-bind="$attrs"
+		@click="$emit('click', $event)"
+	>
+		<template v-if="$slots['icon-left']" #icon-left>
+			<slot name="icon-left" />
+		</template>
+		<template v-if="$slots['icon-right']" #icon-right>
+			<slot name="icon-right" />
+		</template>
+	</BaseButton>
+</template>
