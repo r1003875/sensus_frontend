b34e0de81959813abe74ab67479c1a07fbad5054
Simon Van den Eynde <r1003875@student.thomasmore.be>
2026-06-02 09:55:18 +0200

    feat: update FAQ section with additional information and improve layout

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -32,13 +32,24 @@ const goTerms = () => router.push({ path: '/gebruikersvoorwaarden', query: { bac
       <h1>FAQ</h1>
       <h2>Hoe werkt deze tool?</h2>
       <p>Om meer te leren over grenzen en consent kun je in deze applicatie scenario's starten. Er zijn verschillende scenario's over verschillende onderwerpen.</p>
+      <p>In die scenario’s moet je vragen beantwoorden en situaties beoordelen. Jouw keuzes kunnen een impact hebben op het gevolg van het scenario.</p>
       <h2>Waarom moet ik mijn leeftijd en gender invullen?</h2>
       <p>We gebruiken deze informatie uitsluitend voor analytische doeleinden. Zo krijgen we een beter beeld van het gedrag van jongeren met betrekking tot consent.</p>
+      <p>Je deelname blijft dus anoniem.</p>
       <h2>Contact</h2>
+      <p>Heb je vragen of feedback?</p>
       <p>Neem contact met ons op via <a href="mailto:sensus-app@outlook.com">sensus-app@outlook.com</a></p>
-      <p>
-        <a href="#" @click.prevent="goPrivacy">privacybeleid</a> - <a href="#" @click.prevent="goTerms">gebruikersvoorwaarden</a>
+      <p class="small-text">
+        <a href="#" @click.prevent="goPrivacy">privacybeleid</a> <a href="#" @click.prevent="goTerms">gebruikersvoorwaarden</a>
       </p>
     </section>
   </main>
 </template>
+
+<style scoped>
+.small-text {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 2rem;
+}
+</style>
